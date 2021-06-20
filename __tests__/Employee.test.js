cc7ef53b9b0d7644d22f3dac5bf012c1dec39ab5
Mike <mcewen1984@hotmail.com>
2021-06-20 18:55:59 -0400

    testing arrays

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -2,10 +2,9 @@ const Employee = require("../lib/Employee");
 
 // jest.mock("../lib/Employee");
 
-test("This is a test to make sure Employee works", () => {
+test("Make sure Employee works", () => {
     const employee = new Employee("sam", 1, "gbnn;m");
-    // console.log(employee);
-    // const e = new employee ('sam')
+    
     expect(employee.name).toEqual(expect.any(String));
     expect(employee.getName()).toBe("sam");
 });
---
+++ index.js
@@ -0,0 +1,24 @@
+let employeeArr = [
+    {
+    name: "name",
+    id: "id",
+    phone: "phone",
+    getRole()
+
+}
+]
+
+const managerArr = employeeArr.filter(employee => employee.getRole() === "Manager");
+const managerArr = employeeArr.filter(employee => employee.getRole() === "Manager");
+const managerArr = employeeArr.filter(employee => employee.getRole() === "Manager");
+
+
+managerArr.map(manager => {
+    return `<div class="card">${manager.getName()}`
+})
+managerArr.map(manager => {
+    return `<div class="card">${manager.getName()}`
+})
+managerArr.map(manager => {
+    return `<div class="card">${manager.getName()}`
+})
